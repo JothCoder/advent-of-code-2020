67d9f3533e4d92d6b74718f926cf4940f9ec25d7
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-01 22:03:27 +0100

    [py] Prepare day 2

---
+++ python/day02.py
@@ -0,0 +1,20 @@
+import aoc
+
+
+def main():
+    raw_input = aoc.get_input(2)
+    numbers = list(map(int, raw_input.splitlines()))
+
+    print('Part 1:', part1())
+    # print('Part 2:', part2())
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+main()
