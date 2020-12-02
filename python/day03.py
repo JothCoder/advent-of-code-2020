506a5e1abac75806e27096aba6790c6adc80c067
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-02 22:08:53 +0100

    [py] Prepare day 3

---
+++ python/day03.py
@@ -0,0 +1,24 @@
+import aoc
+
+
+def main():
+    raw_input = aoc.get_input(3)
+    parsed_input = list(map(parse_input, raw_input.splitlines()))
+
+    print('Part 1:', part1(parsed_input))
+    # print('Part 2:', part2(parsed_input))
+
+
+def parse_input(input):
+    pass
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+main()
