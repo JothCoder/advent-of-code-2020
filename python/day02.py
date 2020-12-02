767873f8450825d5b624bdf0470cfd6968b9d4c5
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-02 07:46:50 +0100

    [py] Improve day 2

--- python/day02.py
+++ python/day02.py
@@ -3,31 +3,31 @@ import aoc
 
 def main():
     raw_input = aoc.get_input(2)
-    passwords = list(map(parse_input, raw_input.splitlines()))
+    parsed_input = list(map(parse_input, raw_input.splitlines()))
 
-    print('Part 1:', part1(passwords))
-    print('Part 2:', part2(passwords))
+    print('Part 1:', part1(parsed_input))
+    print('Part 2:', part2(parsed_input))
 
 
-def parse_input(raw):
-    left, right = raw.split(':')
-    ns, char = left.split()
+def parse_input(raw_input):
+    policy, password = raw_input.split(':')
+    ns, char = policy.split()
     n1, n2 = ns.split('-')
-    return (int(n1), int(n2), char, right.strip())
+    return (int(n1), int(n2), char, password.strip())
 
 
 def part1(input):
     valids = 0
-    for (n1, n2, char, pwd) in input:
-        if n1 <= pwd.count(char) <= n2:
+    for (n1, n2, char, password) in input:
+        if n1 <= password.count(char) <= n2:
             valids += 1
     return valids
 
 
 def part2(input):
     valids = 0
-    for (n1, n2, char, pwd) in input:
-        if (pwd[n1-1] == char or pwd[n2-1] == char) and not (pwd[n1-1] == char and pwd[n2-1] == char):
+    for (n1, n2, char, password) in input:
+        if (password[n1-1] == char) ^ (password[n2-1] == char):
             valids += 1
     return valids
 
