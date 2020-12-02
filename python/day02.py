3464eeb710b68c0e4a79bdbb84692ae5f167ccad
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-02 07:11:49 +0100

    [py] Solve day 2, part 1 & 2

--- python/day02.py
+++ python/day02.py
@@ -3,18 +3,33 @@ import aoc
 
 def main():
     raw_input = aoc.get_input(2)
-    numbers = list(map(int, raw_input.splitlines()))
+    passwords = list(map(parse_input, raw_input.splitlines()))
 
-    print('Part 1:', part1())
-    # print('Part 2:', part2())
-
-
-def part1():
-    pass
+    print('Part 1:', part1(passwords))
+    print('Part 2:', part2(passwords))
 
 
-def part2():
-    pass
+def parse_input(raw):
+    left, right = raw.split(':')
+    ns, char = left.split()
+    n1, n2 = ns.split('-')
+    return (int(n1), int(n2), char, right.strip())
+
+
+def part1(input):
+    valids = 0
+    for (n1, n2, char, pwd) in input:
+        if n1 <= pwd.count(char) <= n2:
+            valids += 1
+    return valids
+
+
+def part2(input):
+    valids = 0
+    for (n1, n2, char, pwd) in input:
+        if (pwd[n1-1] == char or pwd[n2-1] == char) and not (pwd[n1-1] == char and pwd[n2-1] == char):
+            valids += 1
+    return valids
 
 
 main()
