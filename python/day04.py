216c9a21947914e29bdde7be59a36beb1fb0bcbd
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-03 22:26:45 +0100

    [py] Prepare day 4

---
+++ python/day04.py
@@ -0,0 +1,24 @@
+import aoc
+
+
+def main():
+    raw_input = aoc.get_input(4)
+    parsed_input = list(map(parse_input, raw_input.splitlines()))
+
+    print('Part 1:', part1(parsed_input))
+    # print('Part 2:', part2(parsed_input))
+
+
+def parse_input(input_):
+    pass
+
+
+def part1(input_):
+    pass
+
+
+def part2(input_):
+    pass
+
+
+main()
