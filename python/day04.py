7d1ba538d00c4ff2b453c6a18e9def85d5bad0dc
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-04 19:18:47 +0100

    [py] Improve day 4

--- python/day04.py
+++ python/day04.py
@@ -1,60 +1,57 @@
-import aoc
 import re
+import aoc
+
+
+def validate_height(val):
+    res = re.match(r'(\d+)(cm|in)', val)
+    return res is not None and HEIGHT_VALIDATORS[res.group(2)](int(res.group(1)))
+
+
+FIELD_VALIDATORS = {
+    'byr': lambda x: 1920 <= int(x) <= 2002,
+    'iyr': lambda x: 2010 <= int(x) <= 2020,
+    'eyr': lambda x: 2020 <= int(x) <= 2030,
+    'ecl': lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+    'hcl': lambda x: re.match(r'^#[a-f0-9]{6}$', x),
+    'pid': lambda x: re.match(r'^\d{9}$', x),
+    'cid': lambda _: True,
+    'hgt': validate_height,
+}
+
+HEIGHT_VALIDATORS = {
+    'cm': lambda h: 150 <= h <= 193,
+    'in': lambda h: 59 <= h <= 76,
+}
+
+REQUIRED_FIELD_KEYS = set(FIELD_VALIDATORS.keys()) - {'cid'}
 
 
 def main():
-    raw_input = aoc.get_input(4)
-    parsed_input = list(map(parse_input, raw_input.split('\n\n')))
+    batch_file = aoc.get_input(4)
+    passports = list(map(parse_passport, batch_file.split('\n\n')))
 
-    print('Part 1:', part1(parsed_input))
-    print('Part 2:', part2(parsed_input))
-
-
-def parse_input(input_):
-    return re.compile(r'(\w{3}):(\S+)').findall(input_)
+    print('Part 1:', part1(passports))
+    print('Part 2:', part2(passports))
 
 
-def part1(input_):
-    keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
-    invalids = 0
-    for data in input_:
-        for key in keys:
-            for k in map(lambda x: x[0], data):
-                if k == key:
-                    break
-            else:
-                if key == 'cid':
-                    continue
-                else:
-                    invalids += 1
-                    break
-    return len(input_) - invalids
+def parse_passport(raw_passport):
+    return dict(re.compile(r'(\w{3}):(\S+)').findall(raw_passport))
 
 
-def part2(input_):
-    keys = [
-        ('byr', lambda x: 1920 <= int(x) <= 2002),
-        ('iyr', lambda x: 2010 <= int(x) <= 2020),
-        ('eyr', lambda x: 2020 <= int(x) <= 2030),
-        ('hgt', lambda x: 150 <= int(x[:-2]) <= 193 if x[-2:] == 'cm' else 59 <= int(x[:-2]) <= 76 if x[-2:] == 'in' else False),
-        ('hcl', lambda x: re.match(r'^#[a-f0-9]{6}$', x)),
-        ('ecl', lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']),
-        ('pid', lambda x: re.match(r'^\d{9}$', x)),
-        ('cid', lambda x: True)
-        ]
-    invalids = 0
-    for data in input_:
-        for key, check in keys:
-            for k, v in data:
-                if k == key and check(v):
-                    break
-            else:
-                if key == 'cid':
-                    continue
-                else:
-                    invalids += 1
-                    break
-    return len(input_) - invalids
+def part1(passports):
+    return sum(has_required_keys(x) for x in passports)
+
+
+def part2(passports):
+    return sum(has_required_keys(x) and validate_values(x) for x in passports)
+
+
+def has_required_keys(fields):
+    return len(REQUIRED_FIELD_KEYS - set(fields.keys())) == 0
+
+
+def validate_values(fields):
+    return not any(not FIELD_VALIDATORS[k](fields[k]) for k in REQUIRED_FIELD_KEYS)
 
 
 main()
