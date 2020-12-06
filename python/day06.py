553193350401e6c2a5fbc875e4b82be7fa8e97e6
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-06 08:44:05 +0100

    [py] Improve day 6

--- python/day06.py
+++ python/day06.py
@@ -1,36 +1,29 @@
+from functools import reduce
 import aoc
 
 
 def main():
     raw_input = aoc.get_input(6)
-    parsed_input = list(map(parse_input, raw_input.split('\n\n')))
+    parsed_input = list(map(lambda x: x.splitlines(), raw_input.split('\n\n')))
 
     print('Part 1:', part1(parsed_input))
     print('Part 2:', part2(parsed_input))
 
 
-def parse_input(input_):
-    return input_.splitlines()
-
-
 def part1(input_):
-    count = 0
-    for group in input_:
-        collection = set()
-        for line in group:
-            collection = collection | set(line)
-        count += len(collection)
-    return count
+    return sum(len(questions_answered(x)) for x in input_)
 
 
 def part2(input_):
-    count = 0
-    for group in input_:
-        collection = set(group[0])
-        for line in group[1:]:
-            collection = collection & set(line)
-        count += len(collection)
-    return count
+    return sum(len(questions_answered_by_everyone(x)) for x in input_)
+
+
+def questions_answered(group):
+    return reduce(lambda st, x: st | set(x), group, set())
+
+
+def questions_answered_by_everyone(group):
+    return reduce(lambda st, x: st & set(x), group[1:], set(group[0]))
 
 
 main()
