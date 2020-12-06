4b6ec2efa172724b56c88e9e86be78ab9e572779
JothCoder <41806047+JothCoder@users.noreply.github.com>
2020-12-06 08:29:26 +0100

    [py] Solve day 6, part 1 & 2

--- python/day06.py
+++ python/day06.py
@@ -3,22 +3,34 @@ import aoc
 
 def main():
     raw_input = aoc.get_input(6)
-    parsed_input = list(map(parse_input, raw_input.splitlines()))
+    parsed_input = list(map(parse_input, raw_input.split('\n\n')))
 
     print('Part 1:', part1(parsed_input))
     print('Part 2:', part2(parsed_input))
 
 
 def parse_input(input_):
-    return input_
+    return input_.splitlines()
 
 
 def part1(input_):
-    return None
+    count = 0
+    for group in input_:
+        collection = set()
+        for line in group:
+            collection = collection | set(line)
+        count += len(collection)
+    return count
 
 
 def part2(input_):
-    return None
+    count = 0
+    for group in input_:
+        collection = set(group[0])
+        for line in group[1:]:
+            collection = collection & set(line)
+        count += len(collection)
+    return count
 
 
 main()
